0be643dc251c439047684d8c946c2ea6f9c719f7
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2022-10-20 22:15:28 +0200

    Exercice1.1
    Création de la branche Develop
    
    +
    
    choix d'un nombre aléatoire et demande d'un nombre à l'utilisateur

---
+++ exercice1/exo1.js
@@ -0,0 +1,18 @@
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
+
+resultat = getRandomInt(99);
+
+function demandeNombre(){
+    while(true){
+        number = parseInt(prompt("Entreez un nombre"))
+        if(isNaN(number))[
+            alert(`${number} n'est pas un nombre `)
+        ]
+        console.log(`Votre nombre est : ${number}`)
+        return number;
+    }
+    
+}
+demandeNombre();
